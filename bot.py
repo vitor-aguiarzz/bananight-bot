7e483dcd15791d7132613648ea7e565f4c14eb6c
vitor-aguiarzz <galaxiustraveler@outlook.com>
2022-07-13 19:49:12 -0300

    bot edit

--- bot.py
+++ bot.py
@@ -1,46 +1,53 @@
+from itertools import count
 import tweepy
 import time
 import random
+import credentials
 
 from os import environ
-API_KEY = environ['API_KEY']
-API_SECRET = environ['API_SECRET']
-ACCESS_TOKEN = environ['ACCESS_TOKEN']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+# API_KEY = environ['API_KEY']
+# API_SECRET = environ['API_SECRET']
+# ACCESS_TOKEN = environ['ACCESS_TOKEN']
+# ACCESS_SECRET = environ['ACCESS_SECRET']
 
-authenticator = tweepy.OAuthHandler(API_KEY, API_SECRET)
-authenticator.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+authenticator = tweepy.OAuthHandler(credentials.API_KEY, credentials.API_SECRET)
+authenticator.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_SECRET)
 
 api = tweepy.API(authenticator)
 
 terms = ["bananight", "Bananight"]
 INTERVAL = 60 * 3 # tweet every 3 minutes
 
-f = open('temas.txt', 'r')
-temas = f.read().splitlines
+f = open('temas.txt', 'r', encoding='utf-8')
+temas = f.readlines()
 f.close
 
 def get_tema():
-    return temas[random.randint(0, len(temas) -1)]
+    print(temas)
+    tamanho = len(temas)
+    return temas[random.randint(0, tamanho - 1)].rstrip()
 
 
 while True:
-    for tweet in tweepy.Cursor(api.search_tweets, q = terms).items(20):
-        status = api.get_status(tweet.id)
-        favorited = status.favorited 
-        if favorited == True:
-            print("ja foi favoritado")
-        else:
-            api.create_favorite(tweet.id)
-            print("novo tweet")
+    # for tweet in tweepy.Cursor(api.search_tweets, q = terms).items(20):
+    #     status = api.get_status(tweet.id)
+    #     favorited = status.favorited 
+    #     if favorited == True:
+    #         print("ja foi favoritado")
+    #     else:
+    #         api.create_favorite(tweet.id)
+    #         print("novo tweet")
     
-    for tweet in tweepy.Cursor(api.search_tweets, q = "@bananight_bot").items(10):
+    for tweet in tweepy.Cursor(api.search_tweets, q = "@bananight_bot", count = 10).items(50):
         repliedStatus = api.get_status(tweet.id)
-        isReplied = status.favorited
-        if favorited == True:
-            print("Ja foi respondido")
+        isReplied = repliedStatus.favorited
+        if isReplied == True:
+            print(tweet.text)
+            break
         else:
             api.create_favorite(tweet.id)
-            api.update_status("Bananight Edição " + get_tema(), tweet.id)
-    api.update_status("Bananight Edição " + get_tema())
-    time.sleep(INTERVAL)
+            print(tweet.text)
+            print("respondi um novo")
+            api.update_status(status = ' Bananight Edição ' + get_tema(), in_reply_to_status_id = tweet.id , auto_populate_reply_metadata=True)
+            break
+    # api.update_status("Bananight Edição " + get_tema())
